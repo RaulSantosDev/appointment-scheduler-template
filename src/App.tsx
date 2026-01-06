8ad5721b442a826fe1a1fd82d85ba5597b689e63
RaulSantosDev <mrpucky27@gmail.com>
2026-01-05 18:27:45 -0600

    Rompiendo el codigo para prueba

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { RouterProvider } from "react-router-dom"
 import { router } from "./app/router"
 
 export default function App() {
-  return < RouterProvider router={ router} />
   // return (
   //   <div className="bg-bg text-text min-h-screen">
   //     <Header />
